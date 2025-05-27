a3f4ac9c6a6f589c94a36ab9e718cc51a452e3a0
Katerina Pustovalova <51330147+Luthineth@users.noreply.github.com>
2025-05-27 22:02:39 +0300

    fix(App): microfix

--- src/components/VFindNumber.vue
+++ src/components/VFindNumber.vue
@@ -134,7 +134,18 @@
             </div>
         </div>
 
-        
+        <div
+            class="flex items-center justify-between message--warning"
+            style="display: flex; justify-content: center; align-items: center; background: white;"
+        >
+          <i
+              class="fa fa-exclamation-circle"
+              aria-hidden="true"
+              style="margin-right: 4px; padding-top: 2px;"
+          ></i>
+          Важно! Все запросы автоматически удаляются спустя 2 часа после скачивания.
+        </div>
+
         <div class="items head-item">
             <div class="item select-none" style="height: 35px">
               <div class="item__header item-title">Номер</div>
@@ -144,17 +155,6 @@
               <div class="item__header delete-icon"></div>
             </div>
         </div>
-      <div
-          class="flex items-center justify-between message--warning"
-          style="display: flex; justify-content: center; align-items: center; background: white;"
-      >
-        <i
-            class="fa fa-exclamation-circle"
-            aria-hidden="true"
-            style="margin-right: 4px; padding-top: 2px;"
-        ></i>
-        Важно! Все запросы автоматически удаляются спустя 2 часа после скачивания.
-      </div>
         
         <div class="items">
             <div class="item" v-for="query in query_list" :key="query.query_id">
