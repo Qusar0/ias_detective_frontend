484dfa80cea300f4ac149b6ae7fe67c167f4981b
helennn123433 <lenomeltashevich@gmail.com>
2026-04-03 21:33:27 +0300

    add refresh token

--- src/main.js
+++ src/main.js
@@ -7,6 +7,34 @@ import 'vue3-toastify/dist/index.css';
 import VueTelInput from 'vue-tel-input';
 import 'vue-tel-input/vue-tel-input.css';
 
+const _originalFetch = window.fetch.bind(window);
+let _refreshPromise = null;
+
+window.fetch = async function (input, init) {
+  const response = await _originalFetch(input, init);
+
+  if (response.status !== 401) {
+    return response;
+  }
+  if (!_refreshPromise) {
+    _refreshPromise = _originalFetch('/api/v1/auth/refresh', {
+      method: 'POST',
+      credentials: 'include',
+      headers: { accept: 'application/json' }
+    })
+      .then(r => r.ok)
+      .catch(() => false)
+      .finally(() => { _refreshPromise = null; });
+  }
+
+  const refreshed = await _refreshPromise;
+  if (refreshed) {
+    return _originalFetch(input, init);
+  }
+
+  return response;
+};
+
 Number.prototype.push_space = function () {
   return [...[...this.toString()].reverse().join('').match(/.{1,3}/g).join(' ')].reverse().join('');
 };
